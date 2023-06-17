52c4c4d8729fa5bf4d49de384ce4f195f63a016d
篠原智哉 <tomoya6032@yahoo.co.jp>
2023-06-17 21:09:44 +0900

    修正が続く。 ChatGPTがいなかったらできなかった

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module GunmaPsw
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 6.0
 
     if Rails.env.development? || Rails.env.test?
       Bundler.require(*Rails.groups)
